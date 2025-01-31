c562b2b5898ca5cd1f3ba21a84782c2623775d40
aadiityaak <aadiityaak@gmail.com>
2025-01-31 14:51:37 +0700

    change task factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,8 +19,8 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         // Generate tanggal dan waktu acak untuk 'start' dan 'end'
-        $start = $this->faker->dateTimeBetween('-1 month', 'now'); // Tanggal mulai dalam 1 bulan terakhir
-        $end = $this->faker->dateTimeBetween($start, '+1 month'); // Tanggal selesai setelah 'start'
+        $start = $this->faker->dateTimeBetween('-1 month', 'now');
+        $end = $this->faker->dateTimeBetween($start, '+5 days');
 
         return [
             'title'         => $this->faker->sentence(),
@@ -26,9 +28,9 @@ class TaskFactory extends Factory
             'start'         => $start,
             'end'           => $end,
             'status'        => $this->faker->randomElement(['ongoing', 'completed', 'cancelled', 'archived']),
-            'category'      => $this->faker->randomElement(['Project', 'Pengembangan', 'Support', 'Training', 'Konsep', 'Lainnya']),
+            'category'      => $this->faker->randomElement(['Project', 'Pengembangan', 'Support', 'Training', 'Konsep', 'Desain', 'Buat Akun', 'Lainnya']),
             'priority'      => $this->faker->randomElement(['low', 'medium', 'high']),
-            'user_id'       => $this->faker->numberBetween(1, 10),
+            'user_id'       => $this->faker->randomElement(User::all())->id,
         ];
     }
 }
